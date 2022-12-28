939f34345999383577f74d37d0a79c303302e814
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-28 12:47:51 -0600

    Implemented style files.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './styles/colors.scss'
+import './styles/fonts.scss'
+import './styles/Mobile.scss'
+import './styles/Desktop.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
