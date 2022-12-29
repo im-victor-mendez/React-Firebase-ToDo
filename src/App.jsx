d465ddf0b5a38b5aa3ad82dd9f32e4e32e79e11c
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-28 19:45:59 -0600

    Fixed background style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
 import './App.css'
+/* To add into a changer theme button/function
+import BD_DESKTOP_DARK from "./assets/images/bg-desktop-dark.jpg";
+import BD_DESKTOP_LIGHT from "./assets/images/bg-desktop-light.jpg";
+import BD_MOBILE_DARK from "./assets/images/bg-mobile-dark.jpg";
+import BD_MOBILE_LIGHT from "./assets/images/bg-mobile-light.jpg";
 
 const currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+const currentWidth = window.matchMedia('(min-width: 1024px)').matches
+*/
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(currentTheme)
+  /* To add into a changer theme button/function
+    const [darkTheme, setDarkTheme] = useState(currentTheme)
+
+    backgroundColor: `var(${darkTheme ? '--very-dark-blue' : '--very-light-gray'})`,
+    backgroundImage: `url(${darkTheme ? (currentWidth ? BD_DESKTOP_DARK : BD_MOBILE_DARK) : (currentWidth ? BD_DESKTOP_LIGHT : BD_MOBILE_LIGHT)})`
+  */
   
   return (
-    <div className="App"
-    style={{
-      backgroundColor: darkTheme ? 'var(--very-dark-blue)' : 'var(--very-light-gray)',
-    }}
-    >
+    <div className="App">
     </div>
   )
 }
