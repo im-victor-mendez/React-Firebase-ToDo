e1301f403ff253319c28b2cdc87c056a3275637d
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-01-02 14:06:20 -0600

    Implemented addTodo function to database.

--- src/components/ToDo_Maker/ToDo_Maker.jsx
+++ src/components/ToDo_Maker/ToDo_Maker.jsx
@@ -1,17 +1,41 @@
 import './ToDo_Maker.scss'
-import React from 'react'
+import React, { useState } from 'react'
 
 import ICON_CROSS from '../../assets/images/icon-cross.svg'
+import { uid } from 'uid'
+import { ref, set } from 'firebase/database'
+import { auth, db } from '../../firebase'
 
 {/* To implement addTodo function */}
 function ToDo_Maker() {
+  const [todo, setTodo] = useState('')
+  const [error, setError] = useState(null)
+
+  function handleChange(event) {
+    setTodo(event.target.value)
+  }
+
+  function writeDataBase() {
+    try {
+      const uidd = uid()
+      set(ref(db, `${auth.currentUser.uid}/${uidd}`), { todo, uidd })
+    } catch (error) { setError(error.message) }
+  }
+
   return (
-    <form id='todo_maker'>
+    <form id='todo_maker'
+    onSubmit={(event) => {
+      event.preventDefault()
+      writeDataBase()
+    }}
+    >
+      {/* To structure better :P */}
+      {error && <>{error}</>}
         <button id='todo_maker-button' type='submit'>
             <img src={ICON_CROSS} alt="" />
         </button>
 
-        <input type="text" name="todo" id="todo_maker-input" placeholder='Create a new todo...' />
+        <input type="text" name="todo" id="todo_maker-input" placeholder='Create a new todo...' onChange={handleChange} />
     </form>
   )
 }
