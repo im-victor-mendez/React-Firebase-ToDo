69e88f54dfaf43b3c0f6ab1f95347c711b467061
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-01-02 21:06:11 -0600

    Implemented logout function.

--- src/layouts/Home/Home.jsx
+++ src/layouts/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ToDo_Maker from '../../components/ToDo_Maker/ToDo_Maker'
 import ToDo_Viewer from '../../components/ToDo_Viewer/ToDo_Viewer'
+import { useAuthContext } from '../../context/authContext'
 
 function Home() {
+  const { logout } = useAuthContext()
+
+  function onClickLogout() {
+    logout()
+
+    window.location.reload()
+  }
+
   return (
     <section id='home'>
+        <button id='logout' onClick={onClickLogout}>LogOut</button>
+
         <ToDo_Maker />
 
         <ToDo_Viewer />
