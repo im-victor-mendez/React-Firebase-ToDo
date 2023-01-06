d1e849a1098390c78794e78de119c9c2efa44b40
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-01-05 19:27:07 -0600

    Stydel Header, ToDo_Maker and ToDo_Viewer.

--- src/components/ToDo_Viewer/ToDo_Viewer.jsx
+++ src/components/ToDo_Viewer/ToDo_Viewer.jsx
@@ -30,30 +30,32 @@ function ToDo_Viewer() {
 
   return (
     <section id='todo_viewer'>
+      <div id='todo_viewer-list'>
         { todos && todos.filter(todo => { if(!filter) return todo; return `${todo.status}` === filter }).map(todo => <ToDo key={uid()} uid={todo.uidd} content={todo.todo} status={todo.status} />) }
+      </div>
+      
+      <div id='todo_viewer-filter'>
+        <p id='todo_viewer-items_left'>{ todos && todos.filter(todo => todo.status === false).length } items left</p>
 
-        <div id='todo_viewer-filter'>
-          <p id='todo_viewer-items_left'>{ todos && todos.filter(todo => todo.status === false).length } items left</p>
+        <form id='todo_viewer-filters' onChange={event => setFilter(event.target.value)}>
+          <label className='filter-label'>
+            All
+            <input type="radio" name="filter" className='filter' value={null} />
+          </label>
 
-          <form id='todo_viewer-filters' onChange={event => setFilter(event.target.value)}>
-            <label className='filter-label'>
-              All
-              <input type="radio" name="filter" className='filter' value={null} />
-            </label>
+          <label className='filter-label'>
+            Active
+            <input type="radio" name="filter" className='filter' value={false} />
+          </label>
 
-            <label className='filter-label'>
-              Active
-              <input type="radio" name="filter" className='filter' value={false} />
-            </label>
+          <label className='filter-label'>
+            Completed
+            <input type="radio" name="filter" className='filter' value={true} />
+          </label>
+        </form>
 
-            <label className='filter-label'>
-              Completed
-              <input type="radio" name="filter" className='filter' value={true} />
-            </label>
-          </form>
-
-          <button className='clear' onClick={clearCompleted}>Clear Completed</button>
-        </div>
+        <button className='clear' onClick={clearCompleted}>Clear Completed</button>
+      </div>
     </section>
   )
 }
